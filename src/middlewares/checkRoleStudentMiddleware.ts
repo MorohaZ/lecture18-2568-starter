0c4b54dbc270a8f369b021acc7a562759b1d339c
MorohaZ <chayanat_h@cmu.ac.th>
2025-10-01 15:25:32 +0700

    sdadsdasdas

---
+++ src/middlewares/checkRoleStudentMiddleware.ts
@@ -0,0 +1,26 @@
+import { type Request, type Response, type NextFunction } from "express";
+import { type CustomRequest, type User } from "../libs/types.js";
+import { users, reset_users } from "../db/db.js";
+export const checkRoleStudent = (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  // 1. get "user payload" and "token" from (custom) request
+  const payload = req.user;
+  const token = req.token;
+
+  // 2. check if user exists (search with username)
+  const user = users.find((u: User) => u.username === payload?.username);
+  if (!user || user.role !== "STUDENT") {
+    return res.status(401).json({
+      success: false,
+      message: "Forbidden access",
+    });
+  }
+
+  // (optional) check if token exists in user data
+
+  // Proceed to next middleware or route handler
+  next();
+};
